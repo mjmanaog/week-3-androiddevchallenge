7cae248a13c02cbdf864bdff42839ce3a5cf60ad
MJ Manaog <>
2021-03-13 10:49:15 +0800

    feat: added all syles

--- app/src/main/java/com/example/androiddevchallenge/ui/theme/Theme.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/theme/Theme.kt
@@ -20,26 +20,26 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
-    primary = purple200,
-    primaryVariant = purple700,
-    secondary = teal200
+    primary = green900,
+    secondary = green300,
+    background = gray,
+    surface = white150,
+    onPrimary = Color.White,
+    onSecondary = gray,
+    onSurface = white850
 )
 
 private val LightColorPalette = lightColors(
-    primary = purple500,
-    primaryVariant = purple700,
-    secondary = teal200
-
-        /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
+    primary = pink100,
+    secondary = pink900,
+    surface = white850,
+    onPrimary = gray,
+    onSecondary = Color.White,
+    onBackground = gray,
+    onSurface = gray
 )
 
 @Composable
--- app/src/main/java/com/example/androiddevchallenge/ui/theme/Type.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/theme/Type.kt
@@ -17,27 +17,49 @@ package com.example.androiddevchallenge.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.androiddevchallenge.R
 
-// Set of Material typography styles to start with
+val NunitoSansBold = FontFamily(Font(R.font.nunito_sans_bold))
+val NunitoSansSemiBold = FontFamily(Font(R.font.nunito_sans_semi_bold))
+val NunitoSansLight = FontFamily(Font(R.font.nunito_sans_light))
+
 val typography = Typography(
+    h1 = TextStyle(
+        fontFamily = NunitoSansBold,
+        fontSize = 18.sp,
+        letterSpacing = 0.sp
+    ),
+    h2 = TextStyle(
+        fontFamily = NunitoSansBold,
+        fontSize = 14.sp,
+        letterSpacing = 0.15.sp
+    ),
+    subtitle1 = TextStyle(
+        fontFamily = NunitoSansLight,
+        fontSize = 16.sp,
+        letterSpacing = 0.sp
+    ),
     body1 = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
-    )
-        /* Other default text styles to override
+        fontFamily = NunitoSansLight,
+        fontSize = 14.sp,
+        letterSpacing = 0.sp
+    ),
+    body2 = TextStyle(
+        fontFamily = NunitoSansLight,
+        fontSize = 12.sp,
+        letterSpacing = 0.sp
+    ),
     button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
+        fontFamily = NunitoSansSemiBold,
+        fontSize = 14.sp,
+        letterSpacing = 1.sp
     ),
     caption = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
+        fontFamily = NunitoSansSemiBold,
+        fontSize = 12.sp,
+        letterSpacing = 1.sp
     )
-    */
 )
